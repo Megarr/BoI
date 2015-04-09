103c320d2619f417fd708a885bcecd0981a10693
Megarr <rrtwerdy@gmail.com>
2015-04-09 13:17:52 +0200

    Update ZacSwag.lua

--- ZacSwag.lua
+++ ZacSwag.lua
@@ -11,7 +11,10 @@ if myHero.charName ~= "Zac" then return to end
 function OnLoad()
 	ZacConfig = scriptConfig("Zac SWAG", "ZacSWAG")
 	ZacConfig:addParam("Combo", "Combo mode", SCRIPT_PARAM_ONKEYDOWN, false, 32)
-	ZacConfig:addParam("DrawCircles", "Draw Circles", SCRIPT_PARAM_ONOFF, true)
+	ZacConfig:addParam("DrawQ", "Draw _Q", SCRIPT_PARAM_ONOFF, false)
+	ZacConfig:addParam("DrawW", "Draw _W", SCRIPT_PARAM_ONOFF, false)
+	ZacConfig:addParam("DrawE", "Draw _E", SCRIPT_PARAM_ONOFF, false)
+	ZacConfig:addParam("DrawR", "Draw _R", SCRIPT_PARAM_ONOFF, false)
 	ZacConfig:permaShow("Combo")
 	end
 	-- Execute 10 times per second
@@ -27,13 +30,18 @@ function OnLoad()
 	-- Can we cast Q ?
   if (myHero:CanUseSpell (_Q) == READY) then
 	-- Cast spell on enemy
-	CastSpell(_Q, ts.target.x,ts.target.z)
+	CastSpell(_Q, ts.target.x, ts.target.z)
 	end
 	
 	-- Can we cast W ?
 	if (myHero:CanUseSpell(_W) == READY) then
 	-- Cast spell on enemy
-	CastSpell (_W)
+	CastSpell (_W, ts.target)
+	
+	-- Can we cast E ?
+	if (myHero:CanUseSpell(_E) == READY) then
+	CastSpell (_E, ts.target.x, ts.target.z)
+	end
 	end
 end
 end
@@ -42,5 +50,11 @@ end
 --Drawing graphics
 function OnDraw()
 --Draw circles only if activated on menu
-if (Config.drawCircle) then
-DrawCircle(myHero.x, myHero.y, myHero.z, 650, 0xFFFFF000)
+if (ZacConfig.drawQ) and not myHero.dead then
+DrawCircle(myHero.x, myHero.y, myHero.z, 550, 0xFFFFF000)
+if (ZacConfig.drawW) and not myHero.dead then
+DrawCircle(myHero.x, myHero.y, myHero.z, 350, 0xFFFFF000)
+if (ZacConfig.drawE) and not myHero.dead then
+DrawCircle(myhero.x, myHero.y, myHero.z, 1150,0xFFFFF000)
+if (ZacConfig.drawR) and not myHero.dead then
+DrawCircle(myhero.x, myHero.y, myHero.z, 300, 0xFFFFF000)
